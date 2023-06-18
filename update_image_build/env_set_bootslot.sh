99add8473ae5c1d39a3724be27508b6219b5ed47
turmary <turmary@126.com>
2023-06-18 22:26:22 +0800

    Fixed: hub_(set|print)env to fw_(set|print)env

--- update_image_build/env_set_bootslot.sh
+++ update_image_build/env_set_bootslot.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 echo "Set upgrade_available to 1"
-hub_setenv --upgrade_available 1
+fw_setenv upgrade_available 1
 
 echo "Switch boot_slot to... " 
-cur_boot_slot=$(hub_printenv -f boot_slot)
-if [ x$cur_boot_slot == x"A" ]; then
+cur_boot_slot=$(fw_printenv bootslot)
+if [ x$cur_boot_slot == x"dualA" ]; then
 	echo "Switch to SlotB"
-	hub_setenv --boot_slot 2
+	fw_setenv bootslot dualB
 else
 	echo "Switch to SlotA"
-	hub_setenv --boot_slot 1
+	fw_setenv bootslot dualA
 fi
 
